8660e6f3358515e4ff2e414e524316fbe680180d
kolinabir <knkolin9@gmail.com>
2024-04-04 20:00:11 +0600

    Delete unused files and update .gitignore

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,36 @@
+import Colors from "@/src/constants/Colors";
+import { Image, StyleSheet, Text, View } from "react-native";
+import products from "@/assets/data/products";
+
+const product = products[0];
+
+export default function TabOneScreen() {
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={{ uri: product.image }} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "bold",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+});
