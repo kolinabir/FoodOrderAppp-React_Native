bb3f9198adf0921b45edc6f0b1aec39201932348
kolinabir <knkolin9@gmail.com>
2024-04-04 22:25:33 +0600

    Refactor tab one screen to use ProductListItem component

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,45 @@
+import Colors from "@/src/constants/Colors";
+import { Image, StyleSheet, Text, View } from "react-native";
+import products from "@/assets/data/products";
+import { Product } from "../types/types";
+
+export const defaultPizzaImage =
+  "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/peperoni.png";
+
+type ProductListItemProps = {
+  product: Product;
+};
+
+export const ProductListItem = ({ product }: ProductListItemProps) => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.image}
+        source={{ uri: product.image || defaultPizzaImage }}
+      />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 20,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "600",
+    marginVertical: 10,
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: "bold",
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 1,
+  },
+});
